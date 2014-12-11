c925bd08321cb7706d3c110e5a7d98e1a4c4ecbf
SaurabhKb <saurabhkb@users.noreply.github.com>
2014-12-10 21:30:32 -0500

    added joint consensus to some extent

--- main.go
+++ main.go
@@ -12,23 +12,36 @@ import (
 const MIN_ELECTION_TIMEOUT = 4
 const MAX_ELECTION_TIMEOUT = 6
 
+const DEFAULT_NUM_NODES = 3
+
 func main() {
 	namePtr := flag.String("name", "auto", "replica name")
+	numPtr := flag.Int("n", DEFAULT_NUM_NODES, "number of machines")
 	flag.Parse()
 	util.SetConfigFile("config.txt")
 	_, ServerName, Pid, _, _, HostAddress := util.GetConfigDetailsFromName(*namePtr)
-	EndpointList := util.ReadAllEndpoints(HostAddress)
+
+	EndpointList := util.ReadAllEndpoints(*numPtr)
+	init_nmap := NodeMap{}
+
+	for _, e := range EndpointList {
+		if e == HostAddress {
+			continue
+		}
+		init_nmap.AddNode(util.GetPidFromEndpoint(e), e)
+	}
 
 	rand.Seed(time.Now().UnixNano() / int64(time.Second))
 	TIMER_DURATION := int(float32(MIN_ELECTION_TIMEOUT) + rand.Float32() * float32(MAX_ELECTION_TIMEOUT - MIN_ELECTION_TIMEOUT))
 	util.P_out("TIMER_DURATION: %d", TIMER_DURATION)
 
+	configChangeNotify := make(chan RaftMessage)
 	serverConnector := make(chan string)
 	serverAck := make(chan bool)
 
 	go func() {
 		s := Server{}
-		s.Init(ServerName, Pid, HostAddress, TIMER_DURATION, EndpointList, serverConnector, serverAck)
+		s.Init(ServerName, Pid, HostAddress, TIMER_DURATION, &init_nmap, serverConnector, serverAck, configChangeNotify)
 		s.Start()
 	}()
 
@@ -38,7 +51,24 @@ func main() {
 		fmt.Printf("Input:")
 		fmt.Scanf("%s", &s)
 		util.P_out("read: %s", s)
-		serverConnector <- s
-		<-serverAck
+		if s[0] == '#' {
+			n := int(s[1] - '0')
+			fmt.Println(n)
+			util.SetConfigFile("config.txt")
+
+			EndpointList := util.ReadAllEndpoints(n)
+			nmap := &NodeMap{}
+			for _, e := range EndpointList {
+				if e == HostAddress {
+					continue
+				}
+				nmap.AddNode(util.GetPidFromEndpoint(e), e)
+			}
+			configChangeNotify <- CreateClientSizeRequestMessage("configChange", n, *nmap)
+
+		} else {
+			serverConnector <- s
+			<-serverAck
+		}
 	}
 }
---
+++ node_map.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"raft/util"
+	"strconv"
+)
+
+/*
+* go through the hassle of string keys so that it becomes possible to json serialize
+*/
+type NodeMap map[string]util.Endpoint
+
+func (n *NodeMap) Majority() int {
+	return len(*n) / 2 + 1
+}
+
+func (n *NodeMap) Clear() {
+	for k, _ := range *n {
+		delete(*n, k)
+	}
+}
+
+func (n *NodeMap) AddNode(pid int, endpoint util.Endpoint) {
+	strPid := strconv.Itoa(pid)
+	(*n)[strPid] = endpoint
+}
+
+func (n *NodeMap) GetKeys() []int {
+	l := []int{}
+	for k, _ := range *n {
+		int_k, _ := strconv.Atoi(k)
+		l = append(l, int_k)
+	}
+	return l
+}
+
+func (n *NodeMap) GetNode(pid int) util.Endpoint {
+	strPid := strconv.Itoa(pid)
+	return (*n)[strPid]
+}
+
+type BoolMap map[int]bool
+
+func (b *BoolMap) Set(pid int, val bool) {
+	(*b)[pid] = val
+}
+
+func (b *BoolMap) Clear() {
+	for k, _ := range *b {
+		delete(*b, k)
+	}
+}
+
+func (b *BoolMap) Get(pid int) (bool, bool) {
+	ret, found := (*b)[pid]
+	return ret, found
+}
